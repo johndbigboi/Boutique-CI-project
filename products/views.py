95ddebcd405aa5535428dad7fb4e545c391b4159
john <jericcarreon@yahoo.com>
2020-08-13 00:24:49 +0100

    Added sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -40,7 +40,8 @@ def all_products(request):
                 sortkey = 'lower_name'
                 # Then we annotate the current list of products with a new field.
                 products = products.annotate(lower_name=Lower('name'))
-    #
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 # And check whether the direction is descending in order to decide whether to reverse the order.
