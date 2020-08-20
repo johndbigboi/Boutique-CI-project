4fa6a4d414ac0f8059142add6e4bbe33e46c4634
john <jericcarreon@yahoo.com>
2020-08-20 23:00:04 +0100

    product admin add product view,url and template

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
+
+"""
+We'll also need to update the product detail URL slightly
+to specify that the product ID should be an integer.
+Since otherwise navigating to products /add will interpret the string add as a product id.
+Which will cause that view to throw an error.
+This happens because django will always use the first URL it finds a matching pattern for.
+And in this case unless we specify that product id is an integer
+django doesn't know the difference between a product number and a string like, add.
+I'll also add a trailing slash here as that's just good practice and I've accidentally left it off.
+"""
