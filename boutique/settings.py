8026453faec404daa8fb11977f1febce2d3a34d1
john <jericcarreon@yahoo.com>
2020-08-24 03:04:26 +0100

    Updated stripe wh key

--- boutique/settings.py
+++ boutique/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['django--boutique.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['django--boutique.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
